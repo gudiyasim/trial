1a568d5fedb43110e1d08b0f4dbcb1c067e1719b
Shashi Singh <simran.shashi@gmail.com>
2019-05-28 14:57:56 +0200

    login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+username: string;
+password: string;
+  constructor() { }
+
+  ngOnInit() {
+  }
+  loginUser(){
+  if (this.username == 'Admin'  &&  this.password == 'admin123'){
+      console.log('welcome');
+    }
+  }
+}
